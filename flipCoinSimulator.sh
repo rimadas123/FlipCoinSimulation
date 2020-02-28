4239f7def7721ed1680eee2cf9812b933d123ea2
rimadas123 <rdas5969@gmail.com>
2020-02-28 10:12:52 +0530

    Added : Check tails or heads using selection statement

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,3 +2,13 @@
 
 echo "Welcome to Flip Coin Simulation"
 
+#flipping coin to display head or tail
+
+calTailsOrHeads=$(( RANDOM%2 ))
+if [[ $calTailsOrHeads -eq 1 ]]
+then
+   echo "It's head"
+else
+   echo "It's tail"
+fi
+
